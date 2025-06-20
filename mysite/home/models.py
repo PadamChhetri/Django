236075708bc176277b704b0c655295ee2e6e47a9
Padam Chhetri <padamchhetri710@gmail.com>
2025-06-20 18:49:47 +0545

    Models and Migrate

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+  name=models.CharField(max_length=15)
+  age=models.IntegerField()
+  email=models.EmailField(null=True,blank=True)
+  address=models.TextField(null=True,blank=True)
+  # images=models.ImageField()
+  # file=models.FileField()
+  
+class Product(models.Model):
+  pass
