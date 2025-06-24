0541822a4b2ed06238b4fe2b570980007a25d2a2
Padam Chhetri <padamchhetri710@gmail.com>
2025-06-25 00:36:43 +0545

    ReceipeProject1

--- mysite/veges/views.py
+++ mysite/veges/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def receipes(request):
-  
+
+  if request.method== "POST":
+
+    data=request.POST #fetch data from receipe.html 
+
+    image=request.FILES.get('image')
+    receipe_name=data.get("receipe_name")
+    description=data.get("description")
+
+    Receipe.objects.create(
+      image=image,
+      receipe_name=receipe_name,
+      description=description,
+      )
+    return redirect("/receipes")
   return render(request,"receipe.html")
