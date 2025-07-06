c8847a00fd7a13884d450c22021608a41b2df845
Padam Chhetri <padamchhetri710@gmail.com>
2025-07-06 23:59:16 +0545

    New models

--- mysite/veges/models.py
+++ mysite/veges/models.py
@@ -25,7 +25,11 @@ class StudentID(models.Model):
   student_id=models.CharField( max_length=100)
   def __str__(self)->str:
       return self.student_id
-    
+  
+
+class Subject(models.Model):
+  subject_name=models.CharField( max_length=100)            
+
 class Student(models.Model):
   department=models.ForeignKey(Department, related_name="depart", on_delete=models.CASCADE)
   student_id=models.OneToOneField(StudentID, related_name="studentid", on_delete=models.CASCADE)
@@ -41,3 +45,14 @@ class Student(models.Model):
   class Meta:
     ordering=['student_name']
     verbose_name='student' 
+
+class Subjectmarks(models.Model):
+   student=models.ForeignKey(Student,related_name="studentmarks",on_delete=models.CASCADE)
+   subject=models.ForeignKey(Subject, on_delete=models.CASCADE)
+   marks=models.IntegerField()
+
+   def __str__(self) -> str:
+      return f'{self.student.student_name} {self.subject.subject_name}'
+
+   class Meta:
+      unique_together=['student','subject']
